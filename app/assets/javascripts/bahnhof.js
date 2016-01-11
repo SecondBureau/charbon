804d9b95c1c4af1cddb30b6f2cfea3afaf039635
Gilles Crofils <gilles@secondbureau.com>
2016-01-11 19:27:25 +0800

    CIT 3245 - deployed to heroku - change to uncompressed assets

--- app/assets/javascripts/bahnhof.js
+++ app/assets/javascripts/bahnhof.js
@@ -1,16 +1,16 @@
-//= require frontend/vendor/jquery.min
-//= require frontend/vendor/jquery.flexslider.min
+//= require frontend/vendor/jquery-2.2.0
+//= require frontend/vendor/jquery.flexslider
     
-//= require frontend/vendor/ionic.bundle.min
+//= require frontend/vendor/ionic.bundle
 //= require frontend/vendor/toArrayFilter
-//= require frontend/vendor/angular-endless-scroll.min
+//= require frontend/vendor/angular-endless-scroll
 //= require frontend/vendor/angular-flexslider
     
 //= require frontend/vendor/spin
-//= require frontend/vendor/angular-spinner.min
+//= require frontend/vendor/angular-spinner
     
-//= require frontend/vendor/ui-bootstrap-tpls.min
-//= require frontend/vendor/holder.min
+//= require frontend/vendor/ui-bootstrap-tpls
+//= require frontend/vendor/holder
      
 //= require frontend/desktop/app
 //= require frontend/config-dev
