4b3f7206bb50f93cf1047b74819b27a913f0f0e1
Gilles Crofils <gilles@secondbureau.com>
2016-02-04 15:40:07 +0800

    CIT 3332 bhSticky fixed width

--- app/apps/themes/china_india_dialogue/assets/js/directives/bh-sticky.js
+++ app/apps/themes/china_india_dialogue/assets/js/directives/bh-sticky.js
@@ -1,7 +1,15 @@
 (function() {
   function bhSticky($window) {
+    
     return function($scope, element, attrs) {
       var pageYOffset = parseInt(attrs.bhSticky);
+      var width       = parseInt(attrs.width);
+      
+      function css_width(e, w) {
+        if (isNaN(width))
+          e.css('width', w)
+      }
+      
       angular.element($window).bind('scroll', function() {
         var parent = element.parent();
         var parent_top = parent[0].getBoundingClientRect().top;
@@ -9,17 +17,19 @@
         var parent_width = parent[0].getBoundingClientRect().width;
         var element_bottom = element[0].getBoundingClientRect().bottom;
         var element_height = element[0].getBoundingClientRect().height;
-        
-        element_fixed_width = parent_width - parseInt(parent.css('padding-left')) - parseInt(parent.css('padding-right'))
+        var element_fixed_width = parent_width - parseInt(parent.css('padding-left')) - parseInt(parent.css('padding-right'))
       
         if (parent_top < pageYOffset) {
           if ( parent_bottom <= element_bottom && parent_bottom < pageYOffset + element_height) {
-            element.css('position', 'absolute').css('top', 'auto').css('bottom', '0px').css('width', element_fixed_width);
+            element.css('position', 'absolute').css('top', 'auto').css('bottom', '0px');
+            css_width(element, element_fixed_width);
           } else {
-            element.css('position', 'fixed').css('top', pageYOffset + 'px').css('bottom', 'auto').css('width', element_fixed_width);
+            element.css('position', 'fixed').css('top', pageYOffset + 'px').css('bottom', 'auto');
+            css_width(element, element_fixed_width);
           }
         } else {
-          element.css('position', 'relative').css('top', '0px').css('bottom', 'auto').css('width', '100%');
+          element.css('position', 'relative').css('top', '0px').css('bottom', 'auto');
+          css_width(element, '100%');
         }
         $scope.$apply();
       });
