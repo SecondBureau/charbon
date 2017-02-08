ea34e473ccba662255ed19c45c8e7b83809b6c9e
Gilles Crofils <gilles@secondbureau.com>
2017-02-08 16:35:56 +0800

    CIT 3700 Improve we chat sharing

--- app/apps/themes/china_pictorial/main_helper.rb
+++ app/apps/themes/china_pictorial/main_helper.rb
@@ -140,9 +140,14 @@ module Themes::ChinaPictorial::MainHelper
     require 'barby/barcode/qr_code'
     require 'barby/outputter/png_outputter'
 
-    barcode = Barby::QrCode.new(text, level: :q, size: 5)
-    base64_output = Base64.encode64(barcode.to_png({ xdim: 5 }))
-    "data:image/png;base64,#{base64_output}"
+    #barcode = Barby::QrCode.new(text, level: :q, size: 5)
+    begin
+      barcode = Barby::QrCode.new(text, level: :q, size: 12)
+      base64_output = Base64.encode64(barcode.to_png({ xdim: 5 }))
+      "data:image/png;base64,#{base64_output}"
+    rescue
+      ''
+    end
   end
   
 end
