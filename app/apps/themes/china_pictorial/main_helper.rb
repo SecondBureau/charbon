f9a0ea8d1e20120c80e2b4f7b38a1feb47cad8f2
Gilles Crofils <gilles@secondbureau.com>
2017-02-08 16:16:31 +0800

    CIT 3700 Change QR Library

--- app/apps/themes/china_pictorial/main_helper.rb
+++ app/apps/themes/china_pictorial/main_helper.rb
@@ -1,6 +1,7 @@
 module Themes::ChinaPictorial::MainHelper
   def self.included(klass)
     # klass.helper_method [:my_helper_method] rescue "" # here your methods accessible from views
+    klass.helper_method [:generate_qr]
   end
 
   def china_pictorial_settings(theme)
@@ -132,4 +133,16 @@ module Themes::ChinaPictorial::MainHelper
   def theme_image_tag(source, options={})
     ActionController::Base.helpers.image_tag(theme_asset_path("images/#{source}"), options)
   end
+  
+  def generate_qr(text)
+    require 'barby'
+    require 'barby/barcode'
+    require 'barby/barcode/qr_code'
+    require 'barby/outputter/png_outputter'
+
+    barcode = Barby::QrCode.new(text, level: :q, size: 5)
+    base64_output = Base64.encode64(barcode.to_png({ xdim: 5 }))
+    "data:image/png;base64,#{base64_output}"
+  end
+  
 end
