79e75f066b28440bf6627257817952684a96456a
Gilles Crofils <gilles@secondbureau.com>
2017-10-03 15:27:02 +0800

    Optimized cache

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,12 +3,9 @@ class ApiController < ApplicationController
   prepend_view_path Rails.root.join("app/apps/").to_s
   
   def cp_posts_blocks_thumbnails
-    post = CamaleonCms::Post.find(params[:post_id].to_i)
-    if post.nil?
-      @posts = CamaleonCms::Post.all.to_a.sample(3)
-    else
-      @posts = post.decorate.the_related_posts.where('cama_posts.id != ?', post.id).to_a.sample(3)
-    end
+    @post = CamaleonCms::Post.find(params[:post_id].to_i)
+    return render html: "<b>Reload Page</b>".html_safe if @post.nil?
+    
   end
   
 
